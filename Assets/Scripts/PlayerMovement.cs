92e401d5a45d7c685fc4d68a0977a14a30103fad
Polik263 <a.dominik2001@interia.eu>
2022-11-26 03:15:01 +0100

    2P movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 8f;
+
+    private float horizontal;
+
+
+
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+}
