3eea7f269f6ed612f2fbe3c284c0115072e90f4a
Polik263 <a.dominik2001@interia.eu>
2022-11-26 12:11:19 +0100

    Trying to add knockback

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Knockback : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float KnockBackForce = 2;
+    public float KnockBackForceUp = 10;
+
+
+    
+
+
+}
