b2ad491e7c18e817e9a79cea79fff97e26c185a4
Polik263 <a.dominik2001@interia.eu>
2022-11-27 16:19:31 +0100

    Revert "character movement fixed"
    This reverts commit bfb909754fe299be876b41a5f929a80d0c677c53.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    [SerializeField] private float speed = 800f;
+    public float speed = 8f;
 
-    [SerializeField] private float horizontal;
+    float horizontal;
 
     //public Animator animator;
 
@@ -29,28 +29,28 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        rb.velocity = new Vector2(horizontal * speed * Time.deltaTime, 0);
+
         if (Input.GetKeyDown(KeyCode.D))
-            horizontal = 1;
+        {
+         horizontal = 1;
+        }
 
-         if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A)) 
+        {
             horizontal = -1;
+        }
 
-         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-             horizontal = 0;
+        //rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
-         transform.position += new Vector3(horizontal * speed, 0, 0);
-         //rb.velocity = new Vector2(horizontal * speed * Time.deltaTime, 0);
-
-         //rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-
-         // if(gameOverScreen.activeInHierarchy == true)
-         // {
-         //     gameOver = true;
-         // }
-         // else
-         // {
-         //     gameOver = false;
-         // }
+        // if(gameOverScreen.activeInHierarchy == true)
+        // {
+        //     gameOver = true;
+        // }
+        // else
+        // {
+        //     gameOver = false;
+        // }
     }
 
     //public void Move(InputAction.CallbackContext context)
